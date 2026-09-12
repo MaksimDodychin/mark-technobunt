b2ec80b821bb73aedea1b8065b5cb1b54ba6f2e5
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-12 22:02:31 +0300

    🪐 Панель Планеты игр: кнопка «в лобби» и приближение экрана

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = 'd8193ec1a8';
+const VERSION = 'c5e93b08ab';
 const PREFIX = "technobunt-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
